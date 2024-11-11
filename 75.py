feb7d54fb690512dba14d187b3c125c92b372f79
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2024-11-11 12:02:34 +0530

    Update 75.py

--- 75.py
+++ 75.py
@@ -7,4 +7,4 @@ def check_even_odd(number):
         return "Odd"
 result = check_even_odd(4)
 print(result)
-print("This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("THIS PROGRAM IS WRITTEN BY ANUSHKA GUPTA ERP :- 0221BCA155")
