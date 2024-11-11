a2e68923121a51f1e04176ff83d152ff87278bcd
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2024-11-11 11:45:13 +0530

    Update 15.py

--- 15.py
+++ 15.py
@@ -21,4 +21,4 @@ print(f"a //= 15 -> {a}")
 a **= 15
 print(f"a **= 15 -> {a}")
 
-print("THIS PROGRAM IS WRITTEN BY JAGRIT AHUJA ERP :- 0221BCA142")
+print("THIS PROGRAM IS WRITTEN BY ANUSHKA GUPTA ERP :- 0221BCA155")
