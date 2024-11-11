b640e67962e304528d833e5fa48dfbed63a43588
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2024-11-11 12:03:11 +0530

    Update 79.py

--- 79.py
+++ 79.py
@@ -12,4 +12,4 @@ def display():
     print("Display function executed")
 
 display()
-print("This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("THIS PROGRAM IS WRITTEN BY ANUSHKA GUPTA ERP :- 0221BCA155")
