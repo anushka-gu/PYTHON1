287cee733f1aac4b88eba66eef1fb290af827945
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2024-11-11 12:01:56 +0530

    Update 71.py

--- 71.py
+++ 71.py
@@ -3,4 +3,4 @@ def add(a,b):
     return a+b
 result= add(10,20)
 print(result)
-print("This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("THIS PROGRAM IS WRITTEN BY ANUSHKA GUPTA ERP :- 0221BCA155")
