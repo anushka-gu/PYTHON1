d78c7b40eefb44bc30df84de5d831fa966e4f2d7
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2024-11-11 11:53:24 +0530

    Update 40.py

--- 40.py
+++ 40.py
@@ -23,4 +23,4 @@ print("Replaced Text:", replaced_text)
 print("Index of 'World':", index)
 print("Count of 'o':", count)
 
-print("This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("THIS PROGRAM IS WRITTEN BY ANUSHKA GUPTA ERP :- 0221BCA155")
