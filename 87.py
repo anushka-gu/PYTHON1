1f61818282fbe63fbff679d68a73831addbe28db
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2024-11-11 12:04:31 +0530

    Update 87.py

--- 87.py
+++ 87.py
@@ -3,4 +3,4 @@
 import math_utils as mu 
 result = mu.add(5, 45) 
 print(result)
-print("87. This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("THIS PROGRAM IS WRITTEN BY ANUSHKA GUPTA ERP :- 0221BCA155")
