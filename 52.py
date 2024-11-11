041c43e5a8965d874fa1a372fa82382ccb3b5ba1
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2024-11-11 11:57:08 +0530

    Update 52.py

--- 52.py
+++ 52.py
@@ -4,4 +4,4 @@ my_list = [1, 2, 3, 4]
 it = iter(my_list)
 for item in it:
  print(item) # Outputs: 1, 2, 3, 4
-print("This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("THIS PROGRAM IS WRITTEN BY ANUSHKA GUPTA ERP :- 0221BCA155")
