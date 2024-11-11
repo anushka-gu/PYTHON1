bb29165c3eb0654add387dfeaaee2970f3ea4793
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2024-11-11 12:02:05 +0530

    Update 72.py

--- 72.py
+++ 72.py
@@ -3,4 +3,4 @@ def greet(name="guest"):
     print(f"Hello,{name}!")
 greet()
 greet("Jagrit")
-print("This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("THIS PROGRAM IS WRITTEN BY ANUSHKA GUPTA ERP :- 0221BCA155")
