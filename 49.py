60d49f5ed9c53221ef434c129744f3c07a76d934
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2024-11-11 11:56:22 +0530

    Update 49.py

--- 49.py
+++ 49.py
@@ -4,4 +4,4 @@ squares = [x**2 for x in range(10)]
 even_squares = [x**2 for x in range(10) if x % 2 == 0]
 print("Squares:", squares) # Outputs: [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 print("Even squares:", even_squares) # Outputs: [0, 4, 16, 36, 64]
-print("This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("THIS PROGRAM IS WRITTEN BY ANUSHKA GUPTA ERP :- 0221BCA155")
