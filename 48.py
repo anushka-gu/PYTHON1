d2e5808fe2833e43602891723c09dd3990c8cbc7
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2024-11-11 11:56:12 +0530

    Update 48.py

--- 48.py
+++ 48.py
@@ -7,4 +7,4 @@ sub_list = my_list[1:4]
 print("First element:", first_element) # Outputs: a
 print("Last element:", last_element) # Outputs: e
 print("Sliced list:", sub_list) # Outputs: ['b', 'c', 'd']
-print("This code is written by Jagrit Ahuja ERP- 0221BCA142")
+print("THIS PROGRAM IS WRITTEN BY ANUSHKA GUPTA ERP :- 0221BCA155")
